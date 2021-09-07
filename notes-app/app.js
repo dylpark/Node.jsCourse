550c9f9895073a3451500ba3c271013ca1f18215
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-07 17:04:09 +1000

    Update app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,8 +7,10 @@ const notesFile = require('./notes.js')
 
 const note = notesFile()
 
-const greenMessage = chalk.green.bold('Success')
+const greenMessage = chalk.green.bold('Green')
+const redMessage = chalk.redBright.bold('Red')
 
 console.log(note)
 console.log(greenMessage)
+console.log(redMessage)
     // console.log(validator.isURL('https://dylpark.com'))
