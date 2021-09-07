d719db9108c55266925968a44c510f59b7c4f35c
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-07 15:14:28 +1000

    Update app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,6 +2,5 @@ const fs = require('fs')
 
 // fs.writeFileSync('notes.txt', 'My name is Andrew.')
 
-fs.writeFileSync('notes.txt', 'My name is Dylan')
-
+fs.writeFileSync('notes.txt', 'My name is Andrew')
 fs.appendFileSync('notes.txt', ' I live in Philadelphia.')
