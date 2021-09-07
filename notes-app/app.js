d70b01a50f4291453af27920e4a0da5ba696828c
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-07 16:56:29 +1000

    Testing locally installed packages

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,14 @@
 //
 //
 //
-const validator = require('validator')
+// const validator = require('validator')
+const chalk = require('chalk')
 const notesFile = require('./notes.js')
 
 const note = notesFile()
 
+const greenMessage = chalk.green.bold('Success')
+
 console.log(note)
-console.log(validator.isEmail('@example.com'))
+console.log(greenMessage)
+    // console.log(validator.isURL('https://dylpark.com'))
