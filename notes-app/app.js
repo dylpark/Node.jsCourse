63707e2d874c53464925f130b6fdb173b4c25247
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-08 12:12:45 +1000

    Adding yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,14 +3,14 @@
 //
 // const validator = require('validator')
 const chalk = require('chalk')
-const notesFile = require('./notes.js')
+const notes = require('./notes.js')
 
 const command = process.argv[2]
 
-if (command === 'add') {
+if (command === chalk.green.bold('add')) {
     console.log('Adding Note')
 } else if (command === 'remove') {
-    console.log('Removing Note')
+    console.log(chalk.red.bold('Removing Note'))
 }
 
 console.log(process.argv)
