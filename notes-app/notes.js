02da42db27c1587a3e7dd2c60a5f378fe6fbd9ac
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-10 16:15:46 +1000

    Refactoring to use arrow functions

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,18 +1,13 @@
-//
-//
-//
-const chalk = require('chalk')
 const fs = require('fs')
+const chalk = require('chalk')
 
-const getNotes = function() {
+const getNotes = () => {
     return 'Your notes...'
 }
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function(note) {
-        return note.title === title
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -20,38 +15,35 @@ const addNote = function(title, body) {
             body: body
         })
         saveNotes(notes)
-        console.log(chalk.green.bold('New note added!'))
+        console.log(chalk.green.inverse('New note added!'))
     } else {
-        console.log(chalk.red.bold('Note title taken!'))
+        console.log(chalk.red.inverse('Note title taken!'))
     }
 }
 
-const removeNote = function(title) {
+const removeNote = (title) => {
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function(note) {
-        return note.title !== title
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
     if (notes.length > notesToKeep.length) {
-        console.log(chalk.blue.bold('Note removed'))
+        console.log(chalk.green.inverse('Note removed!'))
         saveNotes(notesToKeep)
     } else {
-        console.log(chalk.red.bold('No note found'))
+        console.log(chalk.red.inverse('No note found!'))
     }
-
 }
 
-const saveNotes = function(notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function() {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
         return JSON.parse(dataJSON)
-    } catch (error) {
+    } catch (e) {
         return []
     }
 }
