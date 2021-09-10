6d5ae9b7b98b1951dec4b5c03560ae144e68383c
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-10 12:27:40 +1000

    Adding a Note

--- notes-app/app.js
+++ notes-app/app.js
@@ -27,8 +27,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -59,5 +58,5 @@ yargs.command({
     }
 })
 
-// yargs.parse()
+yargs.parse()
 console.log(yargs.argv)
