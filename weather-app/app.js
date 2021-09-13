5f5d78f8bbd4f5e07b79f60018639219ed153510
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-13 17:21:16 +1000

    Customising the response

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,12 @@
 const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=API_KEY&query=-27.470125,153.021072'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+
+    // console.log(response.body.current)
+
+    console.log(response.body.current.weather_descriptions[0] +
+        ". It is currently " + response.body.current.temperature + " °C." +
+        " It feels like " + response.body.current.feelslike + " °C."
+    )
 })
