b36196cb42fcd9f2cf45d4a0022112099a6f7c07
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-14 11:55:07 +1000

    Handling errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,27 +1,37 @@
 // Dylan Park, 2021.
 // The Complete Node.js Developer Course (3rd Edition)
 
+const chalk = require('chalk');
 const request = require('postman-request');
-// const weatherURL = 'http://api.weatherstack.com/current?access_key=API_KEY&query=-27.470125,153.021072'
+const weatherURL = 'http://api.weatherstack.com/current?access_key=&query=-27.470125,153.021072'
 
 // request({ url: weatherURL, json: true }, (error, response) => {
-
-//     // console.log(response.body.current)
-
-//     console.log(response.body.current.weather_descriptions[0] +
-//         ". It is currently " + response.body.current.temperature + " °C." +
-//         " It feels like " + response.body.current.feelslike + " °C."
-//     )
+//     if (error) {
+//         console.log(chalk.redBright.bold('Unable to connect to weather provider.'))
+//     } else if (response.body.error) {
+//         console.log(chalk.redBright.bold('Unable to find location.'))
+//     } else {
+//         console.log(chalk.blue.bold(response.body.current.weather_descriptions[0] +
+//             ". It is currently " + response.body.current.temperature + " °C." +
+//             " It feels like " + response.body.current.feelslike + " °C."
+//         ))
+//     }
 // })
 
 // Geocoding HTTP Request
 // Address -> Lat/Long -> Weather
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token='
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Brisbane.json?access_token='
 
 request({ url: geocodeURL, json: true }, (error, response) => {
-    const lat = response.body.features[0].center[1]
-    const lon = response.body.features[0].center[0]
+    if (error) {
+        console.error(chalk.redBright.bold('Unable to connect to location services.'))
+    } else if (response.body.features === 0) {
+        console.error(chalk.redBright.bold('Unable to find location. Try another search term.'))
+    } else {
+        const lat = response.body.features[0].center[1]
+        const lon = response.body.features[0].center[0]
+        console.log(lat, lon)
+    }
 
-    console.log(lat, lon)
 })
