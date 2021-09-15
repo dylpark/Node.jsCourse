172dbfab835cacb325439442f2d22bd963cf5615
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-15 14:21:57 +1000

    Templating and 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,41 +3,45 @@
 
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 // app.com
 // app.com/help
 // app.com/about
-
 const app = express()
 
 // Paths for express config
 const publicDir = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup for handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDir))
 
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
-        name: 'Name'
+        title: 'Index',
+        name: 'Weather Man'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'Weather App',
+        title: 'About Weather App',
         name: 'Weather Man'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        message: 'This is a test help message.'
+        message: 'This is a test help message.',
+        title: 'Help',
+        name: 'Weather Man'
     })
 })
 
@@ -45,6 +49,22 @@ app.get('/weather', (req, res) => {
     res.send({
         location: 'Sydney',
         forecast: 'This is a weather forecast'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Weather Man',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Weather Man',
+        errorMessage: 'Page not found.'
     })
 })
 
