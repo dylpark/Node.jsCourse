4113775559c17a11e0623576fdee9ba1a3a2c907
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-17 13:47:10 +1000

    Creating a new model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -23,3 +23,23 @@ me.save().then(() => {
 }).catch((error) => {
     console.log('Error!', error)
 })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const task = new Task({
+    description: 'Learn the Mongoose library',
+    completed: false
+})
+
+task.save().then(() => {
+    console.log(task)
+}).catch((error) => {
+    console.log(error)
+})
