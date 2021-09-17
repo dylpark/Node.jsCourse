0998ea4b3d05618053c160ac805dd0aa39a46334
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-17 10:03:45 +1000

    Inserting documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,14 +10,27 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true },
-    (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('tasks').insertMany([{
+        description: 'Clean the house',
+        completed: true
+    }, {
+        description: 'Renew inspection',
+        completed: false
+    }, {
+        description: 'Pot plants',
+        completed: false
+    }], (error, result) => {
         if (error) {
-            console.log('Unable to connect to database')
+            return console.log('Unable to insert tasks!')
         }
-        const db = client.db(databaseName)
-        db.collection('users').insertOne({
-            name: 'Greg',
-            age: '27'
-        })
+
+        console.log(result.insertedIds)
     })
+})
