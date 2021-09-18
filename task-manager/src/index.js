aa3a4db39587d2235ed2a2b7bb0ba419bf3f3906
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-18 14:12:39 +1000

    Resource Updating Endpoints: Part I

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -49,6 +49,29 @@ app.get('/users/:id', async(req, res) => {
     }
 })
 
+// Updating User (Singular)
+app.patch('/users/:id', async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Updates' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 // Create Task
 app.post('/tasks', async(req, res) => {
     const task = new Task(req.body)
