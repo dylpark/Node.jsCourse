a0acbd1605afa1f4ea7551e81023b88f410b4c19
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-18 11:50:47 +1000

    Resource Reading Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+// Create User
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
@@ -21,13 +22,65 @@ app.post('/users', (req, res) => {
     })
 })
 
+// Create Task
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
         res.status(201).send(task)
+
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+// Read Users
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Read User (Singular)
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Read Tasks
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Read Task (Singular)
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
