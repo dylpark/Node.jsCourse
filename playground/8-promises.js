b757f80fe4358b2be9d4d075e951f6113c75c615
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-18 11:19:31 +1000

    Promise chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,18 +1,18 @@
 // Dylan Park, 2021.
 // The Complete Node.js Developer Course (3rd Edition)
 
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve([7, 4, 1])
-            // reject('Things went wrong!')
-    }, 2000)
-})
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve([7, 4, 1])
+//             // reject('Things went wrong!')
+//     }, 2000)
+// })
 
-doWorkPromise.then((result) => {
-    console.log('Success!', result)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// doWorkPromise.then((result) => {
+//     console.log('Success!', result)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
 //
 //                               fulfilled (resolve)
@@ -21,3 +21,38 @@ doWorkPromise.then((result) => {
 //                              \
 //                               rejected (reject)
 //
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+// Nested (Bad) Promise Chaining
+// add(1, 2).then((sum) => {
+//     console.log(sum)
+
+//     add(sum, 5).then((sum2) => {
+//         console.log(sum2)
+//     }).catch((e) => {
+//         console.log(e)
+//     })
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+// Promise Chaining
+add(1, 2).then((sum) => {
+    console.log(sum)
+
+    return add(sum, 4)
+
+}).then((sum2) => {
+    console.log(sum2)
+
+}).catch((e) => {
+    console.log(e)
+})
