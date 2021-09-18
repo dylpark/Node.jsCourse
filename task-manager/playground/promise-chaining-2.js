798533a77e932159e544ecbe26e086f029670a24
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-18 12:38:16 +1000

    Async/Await Challenge

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -4,11 +4,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('6145364acd2c6698d43c3d3f', {}).then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('61454fc77a2ba7a479b8d0f5', {}).then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async(id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('614550af82e939a556e8ea11').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -4,11 +4,24 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('61452f93e7d0bb95dbb86137', { age: 17 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 17 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('61452f93e7d0bb95dbb86137', { age: 17 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 17 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+// Same Functionality as Above, Using Async/Await
+const updateAgeAndCount = async(id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('61452f93e7d0bb95dbb86137', 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
