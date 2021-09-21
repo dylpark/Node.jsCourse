fd8d26a68cb081b3d82e95be37e2e948df82c860
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-21 17:01:37 +1000

    Hiding private data

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,7 +5,6 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,10 +16,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const myFunction = async() => {
-    const token = jwt.sign({ _id: '123' }, 'thisismynewapp', { expiresIn: '7 days' })
-    const data = jwt.verify(token, 'thisismynewapp')
-}
-
-myFunction()
