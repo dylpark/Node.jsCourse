38b2c372f9abaeb3f44f65c1e510c4f504c6d871
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-21 15:35:54 +1000

    Auth Tokes and Express Middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,12 +3,17 @@
 
 const express = require('express')
 require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+app.use((req, res, next) => {
+    console.log(req.mehtod, req.path)
+    next()
+})
 
 app.use(express.json())
 app.use(userRouter)
@@ -18,16 +23,15 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const bcrypt = require('bcrypt')
+// Express Middleware
+// Without
+// New Request -> Run Route Handler
+// With
+// New Request -> Do Something/Customise Behaviour -> Run Route Handler
 
 const myFunction = async() => {
-    const password = 'Red123!'
-    const hashedPassword = await bcrypt.hash(password, 8)
-    console.log(password)
-    console.log(hashedPassword)
-
-    const isPatched = await bcrypt.compare(password, hashedPassword)
-    console.log(isPatched)
+    const token = jwt.sign({ _id: '123' }, 'thisismynewapp', { expiresIn: '7 days' })
+    const data = jwt.verify(token, 'thisismynewapp')
 }
 
 myFunction()
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -8,9 +8,11 @@ const userRouter = new express.Router()
 // Create User
 userRouter.post('/users', async(req, res) => {
     const user = new User(req.body)
+
     try {
         await user.save()
-        res.status(201).send()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
@@ -20,7 +22,8 @@ userRouter.post('/users', async(req, res) => {
 userRouter.post('/users/login', async(req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send()
     }
