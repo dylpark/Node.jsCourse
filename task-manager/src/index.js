441dd9ebe91b26e53d489f925786e88950cc61d1
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-21 18:22:21 +1000

    Cascade Delete Tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -20,14 +20,14 @@ app.listen(port, () => {
 const Task = require('./models/task')
 const User = require('./models/user')
 
-const main = async() => {
-    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
+// const main = async() => {
+//     // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
 
-    const user = await User.findById('61498a943a59417d1e8c65db')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
+//     const user = await User.findById('61498a943a59417d1e8c65db')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
 
-main()
+// main()
