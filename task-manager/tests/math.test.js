448361272b3bd4157d439adb30138f6f8c5d3dcf
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-28 11:57:56 +1000

    Testing Asynchronous Code

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,7 @@
 // Dylan Park, 2021.
 // The Complete Node.js Developer Course (3rd Edition)
 
-const { farenheitToCelcius, celciusTofarenheit } = require('../tests/math.js')
+const { farenheitToCelcius, celciusTofarenheit, add } = require('../tests/math.js')
 
 test('Should convert 32 F to 0 C', () => {
     const temp = farenheitToCelcius(32)
@@ -11,4 +11,23 @@ test('Should convert 32 F to 0 C', () => {
 test('Should convert 0 C to 32 F', () => {
     const temp = celciusTofarenheit(0)
     expect(temp).toBe(32)
+})
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(10)
+//         done()
+//     }, 2000)
+// })
+
+test('Should add two numbers', (done) => {
+    add(2, 3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Another add numbers test async/await', async() => {
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
 })
