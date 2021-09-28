7201dcf90ebaea23519c64438f50909bbe26efaa
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-28 15:04:19 +1000

    Mocking libraries

---
+++ task-manager/tests/__mocks__/@mailgun/mail.js
@@ -0,0 +1,18 @@
+// Dylan Park, 2021.
+// The Complete Node.js Developer Course (3rd Edition)
+
+module.exports = function(API_KEY, DOMAIN) {
+    const object2 = {
+        send() {
+
+        }
+    }
+
+    const object1 = {
+        messages() {
+            return object2
+        }
+    }
+
+    return object1
+}
