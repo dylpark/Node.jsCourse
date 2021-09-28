ca2d5215f0049867d1c3bd27d856a28da04318bc
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-28 15:40:45 +1000

    Setup Task Test Suite

---
+++ task-manager/tests/fixtures/db.js
@@ -0,0 +1,28 @@
+// Dylan Park, 2021.
+// The Complete Node.js Developer Course (3rd Edition)
+
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = require('../../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
+}
+
+const setupDatabase = async() => {
+    await User.deleteMany()
+    await new User(userOne).save()
+}
+
+module.exports = {
+    userOneId,
+    userOne,
+    setupDatabase
+}
