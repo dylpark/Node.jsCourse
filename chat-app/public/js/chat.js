bb13fa320809f69b27831a0e0144c100f5cc8f88
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-29 11:21:11 +1000

    Creating Socket.io Events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,4 +1,16 @@
 // Dylan Park, 2021.
 // The Complete Node.js Developer Course (3rd Edition)
 
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message)
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -11,14 +11,20 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirPath))
+app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
-    console.log('New WebSocket Connection')
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
 })
 
 server.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    console.log(`Server is up on port ${port}!`)
 })
