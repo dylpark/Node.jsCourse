3bf312394f0aa84a47bef1b1f6594930f6555da8
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-09-29 10:02:11 +1000

    Implementing Socket.io

---
+++ chat-app/public/js/chat.js
@@ -0,0 +1,4 @@
+// Dylan Park, 2021.
+// The Complete Node.js Developer Course (3rd Edition)
+
+io()
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,15 +2,23 @@
 // The Complete Node.js Developer Course (3rd Edition)
 
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New WebSocket Connection')
+})
+
+server.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
